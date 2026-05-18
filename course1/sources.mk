0e8632f36bc08d5aea817b05c3fc006f61cfc629
Kerellos Emad <kero.e.yanni2002@gmail.com>
2026-05-18 18:51:45 +0300

    Configured build scripts to support course1 targets and compiler macro switches

---
+++ course1/sources.mk
@@ -0,0 +1,17 @@
+#******************************************************************************
+# Copyright (C) 2026 by Kerellos Emad
+#*****************************************************************************
+
+# Target source files for compile execution
+SOURCES = \
+	main.c \
+	memory.c \
+	data.c \
+	course1.c \
+	stats.c
+
+# Include paths for preprocessor lookup
+INCLUDES = \
+	-I. \
+	-I../include/CMSIS \
+	-I../include/msp432
